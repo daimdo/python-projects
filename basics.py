871e330fbb2209a90de58292851328cfa7ad636d
Damian <80364714+daimdo@users.noreply.github.com>
2024-04-04 15:03:25 +0200

    Added Git guide

---
+++ basics.py
@@ -0,0 +1,4 @@
+# Downloading and installing Python
+# Download and install Python 3.x from: "http://www.python.org/download/". Make sure to check the "Add Python 3.5 to PATH" so Python can run in CMD
+# Install VSCode
+
